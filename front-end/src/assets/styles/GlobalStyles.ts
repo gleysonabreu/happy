72973d89ec8495dc1467cc45464e73277d5f5459
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-10-14 10:48:48 -0300

    Finishing design OrphanagesMap

--- front-end/src/assets/styles/GlobalStyles.ts
+++ front-end/src/assets/styles/GlobalStyles.ts
@@ -18,6 +18,40 @@ const GlobalStyles = createGlobalStyle`
   .leaflet-container {
     z-index: 5;
   }
+  .mapPopup .leaflet-popup-content-wrapper{
+    background: rgba(255, 255, 255, 0.8);
+    border-radius: 20px;
+    box-shadow: none;
+  }
+  .mapPopup .leaflet-popup-content{
+    color: #0089A5;
+    font-size: 20px;
+    font-weight: bold;
+    margin: 8px 12px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .mapPopup .leaflet-popup-content a{
+    width: 40px;
+    height: 40px;
+    background-color: #15c3d6;
+    box-shadow: 17.2868px 27.6589px 41.4884px rgba(23, 142, 166, 0.16);
+
+    border-radius: 12px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: relative;
+    top: 0;
+    right: 0;
+    bottom: 0;
+  }
+
+  .mapPopup .leaflet-popup-tip-container {
+    display: none;
+  }
 `;
 
 export default GlobalStyles;
--- front-end/src/pages/OrphanagesMap/index.tsx
+++ front-end/src/pages/OrphanagesMap/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
-
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-
 import {
   PageMap,
   SideMap,
@@ -17,6 +16,13 @@ import {
 } from './styles';
 
 import mapMarkerImg from '../../assets/images/map-marker.svg';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 function OrphanageMap() {
   return (
@@ -48,6 +54,20 @@ function OrphanageMap() {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+
+        <Marker icon={mapIcon} position={[-3.6899674, -38.6005319]}>
+          <Popup
+            closeButton={false}
+            minWidth={250}
+            maxWidth={250}
+            className="mapPopup"
+          >
+            Lar das meninas
+            <Link to="/">
+              <FiArrowRight size={20} color="#fff" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
       <Link to="/">
