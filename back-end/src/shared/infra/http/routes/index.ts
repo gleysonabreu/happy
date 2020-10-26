417b4251e36f2b44df980397839e0fc6e8836182
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-10-26 11:43:33 -0300

    Add auth middlaware

--- back-end/src/shared/infra/http/routes/index.ts
+++ back-end/src/shared/infra/http/routes/index.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
-
 import orphanages from '@modules/orphanages/infra/http/routes/orphanages.routes';
 import imagesRoute from '@modules/orphanages/infra/http/routes/images.routes';
 import usersRoute from '@modules/users/infra/http/routes/users.routes';
 import sessionRoutes from '@modules/users/infra/http/routes/session.routes';
 import forgorPasswordRoutes from '@modules/users/infra/http/routes/forgotpassword.routes';
+import middlewareAuth from '../middlewares/auth';
 
 const routes = Router();
 
-routes.use('/orphanages', orphanages);
-routes.use('/orphanages/image', imagesRoute);
+routes.use('/orphanages', [middlewareAuth], orphanages);
+routes.use('/orphanages/image', [middlewareAuth], imagesRoute);
 routes.use('/users', usersRoute);
 routes.use('/authenticate', sessionRoutes);
 routes.use('/forgotpassword', forgorPasswordRoutes);
